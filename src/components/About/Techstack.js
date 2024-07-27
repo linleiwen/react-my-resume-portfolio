2ca8cc8961b88c0ec8343339d1f5c27de25f0c70
Max Lin <linleiwen@gmail.com>
2024-07-26 20:59:22 -0400

    add hover

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,69 +1,45 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Tooltip, OverlayTrigger } from "react-bootstrap";
 import { DiPython, DiJavascript1, DiReact, DiGit, DiJava } from "react-icons/di";
 import { SiApacheairflow, SiSnowflake, SiDatabricks, SiHtml5, SiCss3, SiScala, SiAmazonaws, SiRedis, SiMongodb, SiPytorch, SiTensorflow, SiSpringboot, SiJenkins, SiApachekafka } from "react-icons/si";
 import { FaAws } from "react-icons/fa";
 
 function Techstack() {
+  const techStacks = [
+    { icon: <DiPython />, name: "Python" },
+    { icon: <SiApachekafka />, name: "Spark" },
+    { icon: <SiApacheairflow />, name: "Airflow" },
+    { icon: <SiSnowflake />, name: "Snowflake" },
+    { icon: <SiDatabricks />, name: "Databricks" },
+    { icon: <DiReact />, name: "React" },
+    { icon: <SiHtml5 />, name: "HTML" },
+    { icon: <SiCss3 />, name: "CSS" },
+    { icon: <DiJavascript1 />, name: "JavaScript" },
+    { icon: <DiJava />, name: "Java" },
+    { icon: <SiSpringboot />, name: "Spring Boot" },
+    { icon: <SiScala />, name: "Scala" },
+    { icon: <FaAws />, name: "AWS" },
+    { icon: <SiRedis />, name: "Redis" },
+    { icon: <SiMongodb />, name: "MongoDB" },
+    { icon: <SiPytorch />, name: "PyTorch" },
+    { icon: <SiTensorflow />, name: "TensorFlow" },
+    { icon: <DiGit />, name: "Git" },
+    { icon: <SiJenkins />, name: "Jenkins" },
+  ];
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiApachekafka /> {/* 替代 Spark 的通用图标 */}
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiApacheairflow />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSnowflake />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiDatabricks />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiHtml5 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiCss3 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSpringboot />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiScala />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <FaAws />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPytorch />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiTensorflow />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiJenkins />
-      </Col>
+      {techStacks.map((tech, index) => (
+        <OverlayTrigger
+          key={index}
+          placement="top"
+          overlay={<Tooltip id={`tooltip-${index}`}>{tech.name}</Tooltip>}
+        >
+          <Col xs={4} md={2} className="tech-icons">
+            {tech.icon}
+          </Col>
+        </OverlayTrigger>
+      ))}
     </Row>
   );
 }
